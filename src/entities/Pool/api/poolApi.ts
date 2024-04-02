33ca6932a9941a4ed5f1e25551a085c50387ae44
staskoryk <voironlol@gmail.com>
2024-04-02 16:54:57 +0300

    change submit address

--- src/entities/Pool/api/poolApi.ts
+++ src/entities/Pool/api/poolApi.ts
@@ -38,7 +38,7 @@ export const poolApi = baseApi.injectEndpoints({
         }),
         submitTransaction: build.mutation<string, Uint8Array>({
             query: (cbor) => ({
-                url: `http://relaydb.blackrocket.space:8090/api/submit/tx`,
+                url: `https://pool.blackrocket.space/api/submit/tx`,
                 method: 'POST',
                 body: cbor,
                 headers: { 'Content-Type': 'application/cbor' },
