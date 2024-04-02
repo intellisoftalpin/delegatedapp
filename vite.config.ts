5f163ddb45bd056afb3caaaebbfb186b5ee55791
staskoryk <voironlol@gmail.com>
2024-04-02 15:25:38 +0300

    add nodejs polyfills for browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,16 @@
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import svgr from 'vite-plugin-svgr';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
-    plugins: [react(), wasm(), topLevelAwait(), svgr()],
+    plugins: [react(), wasm(), topLevelAwait(), svgr(), nodePolyfills()],
 
     server: {
         proxy: {
             '/submit/tx': 'http://relaydb.blackrocket.space:8090/api/',
-        },
-    },
-
-    optimizeDeps: {
-        esbuildOptions: {
-            // Node.js global to browser globalThis
-            define: {
-                global: 'globalThis',
-            },
-            plugins: [
-                NodeGlobalsPolyfillPlugin({
-                    buffer: true,
-                }),
-            ],
         },
     },
 
